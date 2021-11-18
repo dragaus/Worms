14bbd276b515d0275de248c55c342a466630f7ac
dragaus <dragalichi@gmail.com>
2021-11-18 07:21:29 -0600

    Add Text Mesh Pro and My Network Player

--- Assets/Scripts/Networking/MyNetworkManager.cs
+++ Assets/Scripts/Networking/MyNetworkManager.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 using Mirror;
 public class MyNetworkManager : NetworkManager
 {
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
 
+        var player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Player {numPlayers}");
+    }
 }
---
+++ Assets/Scripts/Player/MyNetworkPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(HandleDisplayText))]
+    [SerializeField]
+    string displayName = "Missing Name";
+
+    [SerializeField] TextMeshPro textMesh;
+
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+
+    private void HandleDisplayText(string oldString, string newString)
+    {
+        textMesh.text = newString;
+    }
+}
